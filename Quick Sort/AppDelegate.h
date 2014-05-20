7cc9d95322d76be2d28ef62c62fa748156bef991
Dan Fairbanks <fairbanksdan@gmail.com>
2014-05-20 15:47:54 -0700

    Initial Commit

---
+++ Quick Sort/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Quick Sort
+//
+//  Created by Daniel Fairbanks on 5/20/14.
+//  Copyright (c) 2014 Fairbanksdan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quick Sort/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quick Sort
+//
+//  Created by Daniel Fairbanks on 5/20/14.
+//  Copyright (c) 2014 Fairbanksdan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
